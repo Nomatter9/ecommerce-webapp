231bde22eba5374862f3ea71656cf3c7f806d00a
mazisi <mazisimsebele18@gmail.com>
2026-01-12 20:35:02 +0200

    fix: database dialect on windows

--- config/database.js
+++ config/database.js
@@ -8,9 +8,6 @@ module.exports = {
     host: process.env.DB_HOST || '127.0.0.1',
     port: process.env.DB_PORT || 3306,
     dialect: 'mysql',
-    dialectOptions: {
-      socketPath: '/var/run/mysqld/mysqld.sock'
-    },
     logging: console.log,
   },
   test: {
